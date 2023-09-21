1d1a7f4d9f531d66210ab3a83ac2876957d37386
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-09-21 12:20:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function loader(){
+	let b=document.getElementById("load");
+	b.innerText="DOM load success";
+}
+document.addEventListener('DOMContentLoaded',loader)
